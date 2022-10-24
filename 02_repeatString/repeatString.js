4b833fbbf7886858edadcb601f25ca0a7a8b9e4b
Kev <kkauf355@gmail.com>
2022-10-23 21:15:26 -0400

    exercises 1 & 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let xStr = ''
+    if (num >= 0) {
+        for (i=0; i<num; i++) {
+            xStr += str;
+        }
+    } else {
+        xStr = 'ERROR'
+    }
+    
+    return xStr;
 };
 
 // Do not edit below this line
