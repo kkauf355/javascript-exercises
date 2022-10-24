daeecbfb1b82bb34fdce87869a6f8220d27f145e
Kev <kkauf355@gmail.com>
2022-10-23 22:03:07 -0400

    exercise 3 passes

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let rev = "";
+    for (i=0; i<str.length; i++) {
+        rev += str[str.length - 1 - i];
+    };
+    return rev;
 };
-
+// conso`le.log(reverseString('hello'));
 // Do not edit below this line
 module.exports = reverseString;
